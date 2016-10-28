e5c85c0f02d263fc791884027e5e211edca01bc4
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-10-28 20:28:57 -0300

    Better test

--- 03-exception-management/exceptions_test.go
+++ 03-exception-management/exceptions_test.go
@@ -1,16 +1,14 @@
 package exceptions_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/eljuanchosf/adventures-in-golang/03-exception-management"
 )
 
 func TestExceptions(t *testing.T) {
-	result, err := exceptions.ThrowException()
+	_, err := exceptions.ThrowException()
 	if err == nil {
 		t.Error("ThrowException should have had returned and exception")
 	}
-	fmt.Sprintf("Returned value: %d", result)
 }
