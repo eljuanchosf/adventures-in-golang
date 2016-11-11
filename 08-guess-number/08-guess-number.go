6bd2e8518f0f7366759d2536f3633644c92fecb5
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-11-11 15:18:47 -0300

    More excercices

---
+++ 08-guess-number/08-guess-number.go
@@ -0,0 +1,60 @@
+/* Write a guessing game where the user has to guess a secret number.
+After every guess the program tells the user whether their number was too large or too small.
+At the end the number of tries needed should be printed.
+I counts only as one try if they input the same number multiple times consecutively. */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type limits struct {
+	bottom int
+	top    int
+}
+
+func main() {
+	limit := limits{1, 10}
+	var guessStack []int
+	numberToGuess := getRandomNumber(limit.bottom, limit.top)
+	fmt.Println(numberToGuess)
+	fmt.Printf("Hello! I have generated a randon number between %d and %d. Can you guess it?\n", limit.bottom, limit.top)
+	fmt.Println("Press CTRL-C to exit at any time!")
+	tries := 1
+	for {
+		userGuess := askForNumber("Please enter a number: ")
+		if userGuess != numberToGuess {
+			if !guessInStack(userGuess, guessStack) {
+				guessStack = append(guessStack, userGuess)
+				tries++
+			}
+		} else {
+			fmt.Printf("Yesss!! You guessed in %d tries!!\n", tries)
+			break
+		}
+	}
+}
+
+func getRandomNumber(bottomLimit int, topLimit int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(topLimit-bottomLimit) + bottomLimit
+}
+
+func guessInStack(guess int, stack []int) bool {
+	for i := 0; i < len(stack); i++ {
+		if stack[i] == guess {
+			return true
+		}
+	}
+	return false
+}
+
+func askForNumber(message string) int {
+	var num int
+	fmt.Printf(message)
+	fmt.Scanf("%d", &num)
+	return num
+}
---
+++ 08-guess-number/08-guess-number_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_guessInStack(t *testing.T) {
+	tests := []struct {
+		name  string
+		stack []int
+		guess int
+		want  bool
+	}{
+		{"Number is in stack", []int{1, 2, 3}, 2, true},
+		{"Number is not in stack", []int{1, 2, 3}, 5, false},
+	}
+	for _, tt := range tests {
+		if got := guessInStack(tt.guess, tt.stack); got != tt.want {
+			t.Errorf("%q. guessInStack(), %d = %v, want %v", tt.name, tt.guess, got, tt.want)
+		}
+	}
+}
+
+func Test_getRandomNumber(t *testing.T) {
+	type args struct {
+		bottomLimit int
+		topLimit    int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"Number is between 1 and 10", args{1, 10}},
+		{"Number is between 20 and 100", args{20, 100}},
+	}
+
+	for _, tt := range tests {
+		got := getRandomNumber(tt.args.bottomLimit, tt.args.topLimit)
+		if got < tt.args.bottomLimit || got > tt.args.topLimit {
+			t.Errorf("getRandomNumber(), bottom %d, top %d, got %d", tt.args.bottomLimit, tt.args.topLimit, got)
+		}
+	}
+}
