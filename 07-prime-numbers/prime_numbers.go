477694c63fe8ff88bb95082af4b46c52c920deef
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-11-11 11:48:12 -0300

    Better algorithm

--- 07-prime-numbers/prime_numbers.go
+++ 07-prime-numbers/prime_numbers.go
@@ -1,17 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 32638; i++ {
+	fmt.Println("Ready to begin generating prime numbers. Press CTRL-C to stop.")
+	time.Sleep(5 * time.Second)
+	var i int64
+	// We know that 1 and 2 are prime numbers by default
+	i = 3
+	for {
 		if isPrime(i) {
-			fmt.Printf("%d\n", i)
+			fmt.Printf("%d, ", i)
 		}
+		// Prime numbers can only be odd, since pair numbers are divisible by 2
+		i += 2
 	}
 }
 
-func isPrime(number int) bool {
-	for index := 2; index <= (number / 2); index++ {
+func isPrime(number int64) bool {
+	var index int64
+	for index = 2; index <= (number / 2); index++ {
 		if number%index == 0 {
 			return false
 		}
--- 07-prime-numbers/prime_numbers_test.go
+++ 07-prime-numbers/prime_numbers_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestIsPrimeNumber(t *testing.T) {
 	type args struct {
-		number int
+		number int64
 	}
 	tests := []struct {
 		name string
@@ -15,8 +15,9 @@ func TestIsPrimeNumber(t *testing.T) {
 		{"3 is prime", args{3}, true},
 		{"10 is not prime", args{10}, false},
 		{"7 is prime", args{7}, true},
-		{"198 is prime", args{198}, false},
+		{"198 is not prime", args{198}, false},
 		{"199 is prime", args{199}, true},
+		{"263201 is prime", args{263201}, true},
 	}
 	for _, tt := range tests {
 		if got := isPrime(tt.args.number); got != tt.want {
