745bb3bffe4fbe3448ec2881a42a4fcdd16f6235
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-12-08 23:15:45 -0300

    Add basic linkedlist

---
+++ linkedlist/list.go
@@ -0,0 +1,79 @@
+package linkedlist
+
+//Cell represents the basic structure of a cell
+type Cell struct {
+	Value int
+	Next  *Cell
+}
+
+//LinkedList represents a linkedList
+type LinkedList struct {
+	Top Cell
+}
+
+//Add adds a new value to the LinkedList
+func (l *LinkedList) Add(value int) {
+	current := &l.Top
+	for current.Next != nil {
+		current = current.Next
+	}
+	if current.Value == 0 && current.Next == nil {
+		current.Value = value
+	} else {
+		current.Next = &Cell{Value: value}
+	}
+}
+
+//InsertAfter inserts a value after the item
+func (l *LinkedList) InsertAfter(item, value int) {
+	current := &l.Top
+	for current.Next != nil {
+		if current.Value == item {
+			tempCell := current.Next
+			current.Next = &Cell{value, tempCell}
+			break
+		}
+		current = current.Next
+	}
+}
+
+//InsertBefore inserts a value after the item
+func (l *LinkedList) InsertBefore(item, value int) {
+	current := &l.Top
+	for current.Next != nil {
+		if current.Next.Value == item {
+			newCell := &Cell{value, current.Next}
+			current.Next = newCell
+			break
+		}
+		current = current.Next
+	}
+}
+
+//RemoveItem inserts a value after the item
+func (l *LinkedList) RemoveItem(item int) {
+	current := &l.Top
+	for current.Next != nil {
+		if current.Next.Value == item {
+			current.Next = current.Next.Next
+			break
+		}
+		current = current.Next
+	}
+}
+
+//RemoveFromStart deletes the first item in the list
+func (l *LinkedList) RemoveFromStart() {
+	l.Top = *l.Top.Next
+}
+
+//ToArray flattens the list to an array
+func (l *LinkedList) ToArray() (arr []int) {
+	current := &l.Top
+	arr = append(arr, current.Value)
+	for current.Next != nil {
+		current = current.Next
+		arr = append(arr, current.Value)
+	}
+	return
+}
---
+++ linkedlist/list_test.go
@@ -0,0 +1,81 @@
+package linkedlist
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/eljuanchosf/adventures-in-golang/shared"
+)
+
+var testArray []int
+
+func buildLinkedList() (l LinkedList) {
+	testArray = []int{23, 17, 8, 44}
+	for _, a := range testArray {
+		l.Add(a)
+	}
+	return
+}
+
+func Test_Add(t *testing.T) {
+	l := buildLinkedList()
+	want := 8
+	if got := l.Top.Next.Next.Value; got != want {
+		t.Errorf("LinkedList.Add(), expected %d, got %d", want, got)
+	}
+}
+
+func Test_ToArray(t *testing.T) {
+	want := testArray
+	l := buildLinkedList()
+	if got := l.ToArray(); !reflect.DeepEqual(got, want) {
+		t.Errorf("LinkedList.ToArray(), expected %d, got %d", want, got)
+	}
+}
+
+func Test_RemoveFromStart(t *testing.T) {
+	l := buildLinkedList()
+	want := testArray[1:]
+	l.RemoveFromStart()
+	if got := l.ToArray(); !reflect.DeepEqual(got, want) {
+		t.Errorf("LinkedList.RemoveFromStart(), expected %d, got %d", want, got)
+	}
+}
+
+func Test_InsertAfter(t *testing.T) {
+	l := buildLinkedList()
+	first := testArray[0:2]
+	last := shared.CopyArray(testArray[2:])
+	want := first
+	want = append(want, 66)
+	want = append(want, last...)
+	l.InsertAfter(17, 66)
+	if got := l.ToArray(); !reflect.DeepEqual(got, want) {
+		t.Errorf("LinkedList.InsertAfter(), expected %d, got %d", want, got)
+	}
+}
+
+func Test_RemoveItem(t *testing.T) {
+	l := buildLinkedList()
+	first := testArray[0:2]
+	last := testArray[3:]
+	want := first
+	want = append(want, last...)
+	l.RemoveItem(8)
+	if got := l.ToArray(); !reflect.DeepEqual(got, want) {
+		t.Errorf("LinkedList.RemoveItem(), expected %d, got %d", want, got)
+	}
+}
+
+func Test_InsertBefore(t *testing.T) {
+	l := buildLinkedList()
+	first := testArray[0:2]
+	last := shared.CopyArray(testArray[2:])
+	want := first
+	want = append(want, 66)
+	want = append(want, last...)
+	l.InsertBefore(8, 66)
+	if got := l.ToArray(); !reflect.DeepEqual(got, want) {
+		t.Errorf("LinkedList.InsertBefore(), expected %d, got %d", want, got)
+	}
+}
